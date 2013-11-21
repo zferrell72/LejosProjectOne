55fd35798c954bf7fb014779921de8fc139ea574
unknown <zferrell@FERREL4609.Student.Northface.local>
2013-11-21 10:15:08 -0700

    Skeletal: 11/21 10:14am

---
+++ src/edu/neumont/csc130/BoundaryDetector.java
@@ -0,0 +1,17 @@
+package edu.neumont.csc130;
+
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+
+public class BoundaryDetector {
+
+	LightSensor lightSensor = new LightSensor(SensorPort.S1);
+	
+	public BoundaryDetector(){
+		lightSensor.calibrateHigh();
+	}
+	
+	public boolean foundEdge(){
+		return (lightSensor.readValue() < (lightSensor.getHigh() / 2));
+	}
+}
---
+++ src/edu/neumont/csc130/CanRemovalBot.java
@@ -0,0 +1,5 @@
+package edu.neumont.csc130;
+
+public class CanRemovalBot {
+
+}
