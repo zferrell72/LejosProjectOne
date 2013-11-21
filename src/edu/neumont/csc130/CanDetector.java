c60f3e74ef9d257a01e632ddf2d55262285bdca6
sinhpah <zacharyhutcherson@gmail.com>
2013-11-21 10:27:11 -0700

    Update CanDetector.java
    Implemented CanDetector

--- src/edu/neumont/csc130/CanDetector.java
+++ src/edu/neumont/csc130/CanDetector.java
@@ -1,5 +1,25 @@
 package edu.neumont.csc130;
 
-public class CanDetector {
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
 
+
+
+public class CanDetector {
+	private UltrasonicSensor ultraSonic = new UltrasonicSensor(SensorPort.S3);
+	private TouchSensor touch = new TouchSensor(SensorPort.S2);
+	private static final int VISUALRANGE = 45;
+	
+	public CanDetector(){
+		ultraSonic.continuous();
+	}
+	
+	public boolean canWithinRange(){
+		return ultraSonic.getDistance() < VISUALRANGE;
+	}
+	
+	public boolean inContact() {
+		return touch.isPressed();
+	}
 }
