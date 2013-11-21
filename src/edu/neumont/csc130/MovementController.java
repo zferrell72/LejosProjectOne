186b550badf4151a1ebe957e79395074a83aa622
rantion <raychacha1234@gmail.com>
2013-11-21 10:30:32 -0700

    HERE IS A MOVEMENT CONTROLLER,IDK IF IT WORKS

--- src/edu/neumont/csc130/MovementController.java
+++ src/edu/neumont/csc130/MovementController.java
@@ -1,5 +1,39 @@
 package edu.neumont.csc130;
 
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTMotor;
+
+
 public class MovementController {
+	
+
+	private NXTMotor leftMotor;
+	private NXTMotor rightMotor;
+	
+	public MovementController(){
+		leftMotor = new NXTMotor(MotorPort.C);
+		rightMotor = new NXTMotor(MotorPort.A);
+	}
+
+	public void turnAround(){
+		rightMotor.setPower(50);
+		rightMotor.forward();
+		
+	}
+	
+	public void moveForward(){
+		rightMotor.forward();
+		leftMotor.forward();
+	}
+	
+	public void moveBackward(){
+		rightMotor.backward();
+		leftMotor.backward();		
+	}
+	
+	public void stopBoth(){
+		rightMotor.stop();
+		leftMotor.stop();
+	}
 
 }
